4176d4e463f7d2f222f59a3250dbf90da479eee9
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-20 22:49:48 -0400

    Creando la entidad Personas

---
+++ PatronRepositorio/Entidades/Personas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Materno { get; set; }
+        public string Paterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public Char Sexo { get; set; }
+        public string Direccion { get; set; }
+
+        public Personas()
+        {
+            PersonaId = 0;
+            DNI = 0;
+            Nombre = string.Empty;
+            Materno = string.Empty;
+            Paterno = string.Empty;
+            FechaNacimiento = DateTime.Now;
+            Telefono = string.Empty;
+            Correo = string.Empty;
+            Sexo = '\0';
+            Direccion = string.Empty;
+        }
+    }
+}
