ba861fa01c1587765bb7c9183ed73967de1355d3
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:36:14 -0400

    Agregando la entidad Marcas

---
+++ PatronRepositorio/Entidades/Marcas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Marcas
+    {
+        [Key]
+        public int MarcaId { get; set; }
+        public string NombreMarca { get; set; }
+
+        public Marcas()
+        {
+            MarcaId = 0;
+            NombreMarca = string.Empty;
+        }
+    }
+}
