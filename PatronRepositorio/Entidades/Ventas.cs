682066b7c9ea8fc8e9bf361febdebad0c4c87298
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:17:08 -0400

    Creando la entidad Ventas

---
+++ PatronRepositorio/Entidades/Ventas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        public int UsuarioId { get; set; }
+        public int ClienteId { get; set; }
+        public int TipoComprobanteId { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public double IGV { get; set; }
+        public double SubTotal { get; set; }
+        public double CostoVenta { get; set; }
+
+        public Ventas()
+        {
+            VentaId = 0;
+            UsuarioId = 0;
+            ClienteId = 0;
+            TipoComprobanteId = 0;
+            FechaVenta = DateTime.Now;
+            IGV = 0;
+            SubTotal = 0;
+            CostoVenta = 0;
+        }
+    }
+}
