ceacc651cecd0ca2d1b89804e7c09224774130d5
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 21:41:04 -0400

    Agregando la entidad Tipos Personas

---
+++ PatronRepositorio/Entidades/TiposPersonas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatronRepositorio.Entidades
+{
+    public class TiposPersonas
+    {
+        [Key]
+        public int TipoPersonaId { get; set; }
+        public string Nombre { get; set; }
+
+        public TiposPersonas()
+        {
+            TipoPersonaId = 0;
+            Nombre = string.Empty;
+        }
+    }
+}
