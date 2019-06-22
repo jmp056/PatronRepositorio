850355acd8c49b72427398f52dbd79927b269e5f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-22 15:32:02 -0400

    Terminando el Test de la entidad Unidads de medida

--- PatronRepositorioTests/Entidades/TiposPersonasTests.cs
+++ PatronRepositorioTests/Entidades/TiposPersonasTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,51 @@ namespace PatronRepositorio.Entidades.Tests
     public class TiposPersonasTests
     {
         [TestMethod()]
-        public void TiposPersonasTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<TiposPersonas> repositorio;
+            repositorio = new RepositorioBase<TiposPersonas>();
+            TiposPersonas tipoPersona = new TiposPersonas();
+            tipoPersona.TipoPersonaId = 1;
+            tipoPersona.Nombre = "Normal";
+            Assert.IsTrue(repositorio.Guardar(tipoPersona));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<TiposPersonas> repositorio;
+            repositorio = new RepositorioBase<TiposPersonas>();
+            TiposPersonas tipoPersona = new TiposPersonas();
+            tipoPersona.TipoPersonaId = 1;
+            tipoPersona.Nombre = "Temporal";
+            Assert.IsTrue(repositorio.Modificar(tipoPersona));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<TiposPersonas> repositorio;
+            repositorio = new RepositorioBase<TiposPersonas>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<TiposPersonas> repositorio;
+            repositorio = new RepositorioBase<TiposPersonas>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<TiposPersonas> repositorio;
+            repositorio = new RepositorioBase<TiposPersonas>();
+            List<TiposPersonas> lista = new List<TiposPersonas>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
--- PatronRepositorioTests/Entidades/UnidadesDeMedidaTests.cs
+++ PatronRepositorioTests/Entidades/UnidadesDeMedidaTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,51 @@ namespace PatronRepositorio.Entidades.Tests
     public class UnidadesDeMedidaTests
     {
         [TestMethod()]
-        public void UnidadesDeMedidaTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<UnidadesDeMedida> repositorio;
+            repositorio = new RepositorioBase<UnidadesDeMedida>();
+            UnidadesDeMedida unidadDeMedida = new UnidadesDeMedida();
+            unidadDeMedida.UnidadDeMedidaId = 1;
+            unidadDeMedida.NombreUnidadDeMedida = "Peso";
+            Assert.IsTrue(repositorio.Guardar(unidadDeMedida));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<UnidadesDeMedida> repositorio;
+            repositorio = new RepositorioBase<UnidadesDeMedida>();
+            UnidadesDeMedida unidadDeMedida = new UnidadesDeMedida();
+            unidadDeMedida.UnidadDeMedidaId = 1;
+            unidadDeMedida.NombreUnidadDeMedida = "Unidad";
+            Assert.IsTrue(repositorio.Modificar(unidadDeMedida));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<UnidadesDeMedida> repositorio;
+            repositorio = new RepositorioBase<UnidadesDeMedida>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<UnidadesDeMedida> repositorio;
+            repositorio = new RepositorioBase<UnidadesDeMedida>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<UnidadesDeMedida> repositorio;
+            repositorio = new RepositorioBase<UnidadesDeMedida>();
+            List<UnidadesDeMedida> lista = new List<UnidadesDeMedida>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
