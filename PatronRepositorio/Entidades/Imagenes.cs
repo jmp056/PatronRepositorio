444eb33d901d4b1234d148aa602c2bb17e46fae9
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:21:09 -0400

    Agregando la entidad Imagenes

---
+++ PatronRepositorio/Entidades/Imagenes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Imagenes
+    {
+        public int ImagenId { get; set; }
+        public string RutaImagen { get; set; }
+
+        public Imagenes()
+        {
+            ImagenId = 0;
+            RutaImagen = string.Empty;
+        }
+    }
+}
