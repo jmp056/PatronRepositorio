ddf91c859dd97c201550521bff7bf36bbb4eb344
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 23:04:48 -0400

    Agregando la entidad Tipos Comprobantes

---
+++ PatronRepositorio/Entidades/TiposComprobantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class TiposComprobantes
+    {
+        public int TipoComprobanteId { get; set; }
+        public string NombreComprobante { get; set; }
+
+        public TiposComprobantes()
+        {
+            TipoComprobanteId = 0;
+            NombreComprobante = string.Empty;
+        }
+    }
+}
