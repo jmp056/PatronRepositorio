bbb156279d665629c130c3a6f5e90fd67f257aed
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 23:59:17 -0400

    Terminando el Test de Categorias

--- PatronRepositorioTests/Entidades/CategoriasTests.cs
+++ PatronRepositorioTests/Entidades/CategoriasTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,53 @@ namespace PatronRepositorio.Entidades.Tests
     public class CategoriasTests
     {
         [TestMethod()]
-        public void CategoriasTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<Categorias> repositorio;
+            repositorio = new RepositorioBase<Categorias>();
+            Categorias categoria = new Categorias();
+            categoria.CategoriaId = 1;
+            categoria.NombreCategoria = "Categoria 1";
+            categoria.Descripcion = "Primera Categoria";
+            Assert.IsTrue(repositorio.Guardar(categoria));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Categorias> repositorio;
+            repositorio = new RepositorioBase<Categorias>();
+            Categorias categoria = new Categorias();
+            categoria.CategoriaId = 1;
+            categoria.NombreCategoria = "Categoria 1";
+            categoria.Descripcion = "1era Categoria";
+            Assert.IsTrue(repositorio.Modificar(categoria));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Categorias> repositorio;
+            repositorio = new RepositorioBase<Categorias>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Categorias> repositorio;
+            repositorio = new RepositorioBase<Categorias>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Categorias> repositorio;
+            repositorio = new RepositorioBase<Categorias>();
+            List<Categorias> lista = new List<Categorias>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
