530e728cd40716f8c5411ee4d12c57064440cf4f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:53:50 -0400

    Agregando la entidad Detalles Compras

---
+++ PatronRepositorio/Entidades/DetallesCompras.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class DetallesCompras
+    {
+        public int DetalleCompraId { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public double Unidades { get; set; }
+        public double Costounidad { get; set; }
+        public double Total { get; set; }
+
+        public DetallesCompras()
+        {
+            DetalleCompraId = 0;
+            CompraId = 0;
+            ProductoId = 0;
+            Unidades = 0;
+            Costounidad = 0;
+            Total = 0;
+        }
+    }
+}
