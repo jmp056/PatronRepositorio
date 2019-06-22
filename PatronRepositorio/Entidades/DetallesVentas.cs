424f1898c206c0cd988b1402800985510b467eed
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:51:05 -0400

    Agregando la entidad DetallesVentas

---
+++ PatronRepositorio/Entidades/DetallesVentas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class DetallesVentas
+    {
+        public int DetalleVentaId { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public double Unidades { get; set; }
+        public double Costounidad { get; set; }
+        public double DescuentoUnidad { get; set; }
+        public double Total { get; set; }
+
+        public DetallesVentas()
+        {
+            DetalleVentaId = 0;
+            VentaId = 0;
+            ProductoId = 0;
+            Unidades = 0;
+            Costounidad = 0;
+            DescuentoUnidad = 0;
+            Total = 0;
+        }
+    }
+}
