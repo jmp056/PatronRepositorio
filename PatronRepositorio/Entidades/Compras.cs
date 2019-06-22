a7ff7ca8afa6fe8031201534fbe176bc534b9b24
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:32:44 -0400

    Agregando la entidad Compras

---
+++ PatronRepositorio/Entidades/Compras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int ProveedorId { get; set; }
+        public int TipoComprobanteId { get; set; }
+        public double CostoCompra { get; set; }
+
+        public Compras()
+        {
+            CompraId = 0;
+            UsuarioId = 0;
+            FechaCompra = DateTime.Now;
+            ProveedorId = 0;
+            TipoComprobanteId = 0;
+            CostoCompra = 0;
+        }
+    }
+}
