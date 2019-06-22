993ee95b841117a564f24327bb62c7c9af40cff9
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-22 00:08:35 -0400

    Terminando el Test de Compras

--- PatronRepositorioTests/Entidades/ComprasTests.cs
+++ PatronRepositorioTests/Entidades/ComprasTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,59 @@ namespace PatronRepositorio.Entidades.Tests
     public class ComprasTests
     {
         [TestMethod()]
-        public void ComprasTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<Compras> repositorio;
+            repositorio = new RepositorioBase<Compras>();
+            Compras compra = new Compras();
+            compra.CompraId = 1;
+            compra.UsuarioId = 1;
+            compra.FechaCompra = DateTime.Now;
+            compra.ProveedorId = 1;
+            compra.TipoComprobanteId = 1;
+            compra.CostoCompra = 0;
+            Assert.IsTrue(repositorio.Guardar(compra));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Compras> repositorio;
+            repositorio = new RepositorioBase<Compras>();
+            Compras compra = new Compras();
+            compra.CompraId = 1;
+            compra.UsuarioId = 1;
+            compra.FechaCompra = DateTime.Now;
+            compra.ProveedorId = 1;
+            compra.TipoComprobanteId = 1;
+            compra.CostoCompra = 100;
+            Assert.IsTrue(repositorio.Modificar(compra));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Compras> repositorio;
+            repositorio = new RepositorioBase<Compras>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Compras> repositorio;
+            repositorio = new RepositorioBase<Compras>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Compras> repositorio;
+            repositorio = new RepositorioBase<Compras>();
+            List<Compras> lista = new List<Compras>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
