c556772b11df425eaf8ce28b97f0d64f9eeb1b1a
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-22 00:05:01 -0400

    Terminando el Test de Clientes

--- PatronRepositorioTests/Entidades/ClientesTests.cs
+++ PatronRepositorioTests/Entidades/ClientesTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,51 @@ namespace PatronRepositorio.Entidades.Tests
     public class ClientesTests
     {
         [TestMethod()]
-        public void ClientesTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<Clientes> repositorio;
+            repositorio = new RepositorioBase<Clientes>();
+            Clientes cliente = new Clientes();
+            cliente.ClienteId = 1;
+            cliente.PersonaId = 0;
+            Assert.IsTrue(repositorio.Guardar(cliente));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Clientes> repositorio;
+            repositorio = new RepositorioBase<Clientes>();
+            Clientes cliente = new Clientes();
+            cliente.ClienteId = 1;
+            cliente.PersonaId = 1;
+            Assert.IsTrue(repositorio.Modificar(cliente));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Clientes> repositorio;
+            repositorio = new RepositorioBase<Clientes>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Clientes> repositorio;
+            repositorio = new RepositorioBase<Clientes>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Clientes> repositorio;
+            repositorio = new RepositorioBase<Clientes>();
+            List<Clientes> lista = new List<Clientes>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
