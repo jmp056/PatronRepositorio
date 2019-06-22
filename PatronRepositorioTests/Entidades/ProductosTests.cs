c1bb522789d90c3143979803241f7db8ead6516a
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-22 00:58:52 -0400

    Terminando el Test de la entidad Productos

--- PatronRepositorioTests/Entidades/ProductosTests.cs
+++ PatronRepositorioTests/Entidades/ProductosTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
 using PatronRepositorio.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,69 @@ namespace PatronRepositorio.Entidades.Tests
     public class ProductosTests
     {
         [TestMethod()]
-        public void ProductosTest()
+        public void Guardar()
         {
-            Assert.Fail();
+            RepositorioBase<Productos> repositorio;
+            repositorio = new RepositorioBase<Productos>();
+            Productos producto = new Productos();
+            producto.ProductoId = 1;
+            producto.Nombre = "Maiz La Famosa";
+            producto.Descripcion = "Lata de 100 mg";
+            producto.FechaFabricacion = DateTime.Now;
+            producto.CostoCompra = 1;
+            producto.Stock = 1;
+            producto.UnidadMedidaId = 1;
+            producto.ImagenId = 1;
+            producto.CategoriaId = 0;
+            producto.MarcaId = 1;
+            producto.ModeloId = 1;
+            Assert.IsTrue(repositorio.Guardar(producto));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Productos> repositorio;
+            repositorio = new RepositorioBase<Productos>();
+            Productos producto = new Productos();
+            producto.ProductoId = 1;
+            producto.Nombre = "Maiz La Famosa";
+            producto.Descripcion = "Lata de 100 mg";
+            producto.FechaFabricacion = DateTime.Now;
+            producto.CostoCompra = 1;
+            producto.Stock = 1;
+            producto.UnidadMedidaId = 2;
+            producto.ImagenId = 1;
+            producto.CategoriaId = 0;
+            producto.MarcaId = 1;
+            producto.ModeloId = 1;
+            Assert.IsTrue(repositorio.Modificar(producto));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Productos> repositorio;
+            repositorio = new RepositorioBase<Productos>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Productos> repositorio;
+            repositorio = new RepositorioBase<Productos>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Productos> repositorio;
+            repositorio = new RepositorioBase<Productos>();
+            List<Productos> lista = new List<Productos>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
