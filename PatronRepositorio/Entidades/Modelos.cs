24465ef15f77f3d4218d38165c4b926017a3c02f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:41:58 -0400

    Agregando la entidad Modelo

---
+++ PatronRepositorio/Entidades/Modelos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Modelos
+    {
+        [Key]
+        public int ModeloId { get; set; }
+        public string NombreModeli { get; set; }
+        public Modelos()
+        {
+            ModeloId = 0;
+            NombreModeli = string.Empty;
+        }
+    }
+}
