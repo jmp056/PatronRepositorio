ebaa0a258b87a18fcc0d35e08949074bee64114f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:03:27 -0400

    Agragando la entidad Cargos

---
+++ PatronRepositorio/Entidades/Cargos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class Cargos
+    {
+        [Key]
+        public int CargoId { get; set; }
+        public string NombreCargo { get; set; }
+        public int EstadoId { get; set; }
+
+        public Cargos()
+        {
+            CargoId = 0;
+            NombreCargo = string.Empty;
+            EstadoId = 0;
+        }
+    }
+}
