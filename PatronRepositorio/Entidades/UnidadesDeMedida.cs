dcc168f45994821424060003f4c059521ee68fe7
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-21 22:38:17 -0400

    Agregando la entidad Unidades De Medida

---
+++ PatronRepositorio/Entidades/UnidadesDeMedida.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.Entidades
+{
+    public class UnidadesDeMedida
+    {
+        [Key]
+        public int UnidadDeMedidaId { get; set; }
+        public string NombreUnidadDeMedida { get; set; }
+
+        public UnidadesDeMedida()
+        {
+            UnidadDeMedidaId = 0;
+            NombreUnidadDeMedida = string.Empty;
+        }
+    }
+}
